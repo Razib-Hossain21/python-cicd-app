181cecfd69a4e732b5c0012aa4afe957fe2dcc36
Razib-Hussain21 <razib.cse1617@gmail.com>
2026-06-20 13:18:56 +0600

    update app file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Mr. Razib welcome to CI/CD project. CI/CD Pipeline Working Successfully..! Thank you"
+    return "Hello Mr. Razib welcome to CI/CD project. CI/CD Pipeline Working Successfully..! Thank yo..! Jenkins Login Info is : Admin, Admin123"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
